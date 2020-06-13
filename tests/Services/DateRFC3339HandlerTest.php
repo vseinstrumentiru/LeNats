21568704a6e0b933ac7be61995fbfa0cfc933944
kim_light <lord.darth.anian@gmail.com>
2020-06-13 12:31:59 +0300

    Added handler for correct processing RFC 3339 formatted date/times

---
+++ tests/Services/DateRFC3339HandlerTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace LeNats\Services;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class DateRFC3339HandlerTest extends TestCase
+{
+    /**
+     * @dataProvider datetimeProvider
+     *
+     * @param string $datetimeRfc3339
+     * @param        $expected
+     *
+     * @throws \ReflectionException
+     */
+    public function testPrepare(string $datetimeRfc3339, $expected)
+    {
+        $handler = new DateRFC3339Handler();
+        $reflection = new ReflectionClass($handler);
+        $method = $reflection->getMethod('prepare');
+        $method->setAccessible(true);
+        $actual = $method->invoke($handler, $datetimeRfc3339);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function datetimeProvider(): array
+    {
+        return [
+            ' 1' => [
+                '2020-06-08T12:46:41+00:00',
+                '2020-06-08T12:46:41+00:00',
+            ],
+            ' 2' => [
+                '2020-06-08T14:50:22.257114',
+                '2020-06-08T14:50:22+00:00',
+            ],
+            ' 3' => [
+                '2020-06-08T14:50:22.257114Z',
+                '2020-06-08T14:50:22+00:00',
+            ],
+            ' 4' => [
+                '2006-12-12T10:01:02.999999999-04:00',
+                '2006-12-12T10:01:02-04:00',
+            ],
+        ];
+    }
+}
