a81a4dc5ecc486f58935f4567472b3672571940d
Falaleev Maxim <maxfalaleev1@gmail.com>
2020-08-26 11:14:42 +0300

    switch string to date immutable

--- tests/Services/DateRFC3339HandlerTest.php
+++ tests/Services/DateRFC3339HandlerTest.php
@@ -33,23 +33,23 @@ class DateRFC3339HandlerTest extends TestCase
         return [
             'RFC3339 formatted date  which php can parse'        => [
                 '2020-06-08T12:46:41+00:00',
-                '2020-06-08T12:46:41+00:00',
+                new \DateTimeImmutable('2020-06-08T12:46:41+00:00'),
             ],
             'RFC3339 formatted date with numsec'                 => [
                 '2020-06-08T14:50:22.257114',
-                '2020-06-08T14:50:22+00:00',
+                new \DateTimeImmutable('2020-06-08T14:50:22+00:00'),
             ],
             'RFC3339 formatted date with numsec and Z'           => [
                 '2020-06-08T14:50:22.257114Z',
-                '2020-06-08T14:50:22+00:00',
+                new \DateTimeImmutable('2020-06-08T14:50:22+00:00'),
             ],
             'RFC3339 formatted date with numsec and time offset' => [
                 '2006-12-12T10:01:02.999999999-04:00',
-                '2006-12-12T10:01:02-04:00',
+                new \DateTimeImmutable('2006-12-12T10:01:02-04:00'),
             ],
             'RFC3339 \DateTimeImmutable'                         => [
                 new \DateTime('2006-12-12T10:01:02-04:00'),
-                '2006-12-12T10:01:02-04:00'
+                new \DateTimeImmutable('2006-12-12T10:01:02-04:00')
             ],
         ];
     }
